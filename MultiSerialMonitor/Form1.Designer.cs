aa8f0d2e4d319c20b6dd066e9731596191307478
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-06-06 19:59:47 +0300

    first commit

---
+++ MultiSerialMonitor/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MultiSerialMonitor
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelBaud;
+        private System.Windows.Forms.Button buttonClear;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelConnection;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.labelBaud = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelConnection = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(108, 22);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(126, 28);
+            this.comboBoxPorts.TabIndex = 0;
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(371, 20);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(168, 28);
+            this.comboBoxBaudRate.TabIndex = 1;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnect.Location = new System.Drawing.Point(559, 18);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(122, 34);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSend.Location = new System.Drawing.Point(20, 60);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(519, 26);
+            this.textBoxSend.TabIndex = 3;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(559, 56);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(122, 34);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.BackColor = System.Drawing.Color.Black;
+            this.richTextBoxLog.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxLog.ForeColor = System.Drawing.Color.White;
+            this.richTextBoxLog.Location = new System.Drawing.Point(20, 100);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ReadOnly = true;
+            this.richTextBoxLog.Size = new System.Drawing.Size(661, 200);
+            this.richTextBoxLog.TabIndex = 5;
+            this.richTextBoxLog.Text = "";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPort.Location = new System.Drawing.Point(20, 23);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(82, 20);
+            this.labelPort.TabIndex = 6;
+            this.labelPort.Text = "COM Port:";
+            // 
+            // labelBaud
+            // 
+            this.labelBaud.AutoSize = true;
+            this.labelBaud.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBaud.Location = new System.Drawing.Point(275, 25);
+            this.labelBaud.Name = "labelBaud";
+            this.labelBaud.Size = new System.Drawing.Size(90, 20);
+            this.labelBaud.TabIndex = 7;
+            this.labelBaud.Text = "Baud Rate:";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(20, 310);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(80, 30);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "Temizle";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelConnection});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 347);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(707, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelConnection
+            // 
+            this.toolStripStatusLabelConnection.Name = "toolStripStatusLabelConnection";
+            this.toolStripStatusLabelConnection.Size = new System.Drawing.Size(86, 17);
+            this.toolStripStatusLabelConnection.Text = "No connection";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(707, 369);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.labelBaud);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Multi Serial Monitor";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ MultiSerialMonitor/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiSerialMonitor
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder receiveBuffer = new StringBuilder(); // Buffer for incoming serial data
+        private DateTime lastReceiveTime;                          // Timestamp of last received data
+        private System.Windows.Forms.Timer receiveTimer;           // Timer to batch received data processing
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new Size(723, 408);
+            this.MaximumSize = new Size(723, 408);
+
+            PopulateAvailablePorts();
+            InitializeReceiveTimer();
+            textBoxSend.KeyDown += TextBoxSend_KeyDown;
+
+            UpdateConnectionStatus(false, null, 0); // There is no connection in the first case
+            buttonSend.Enabled = false; // Initially, the send button is passive
+        }
+
+        /// <summary>
+        /// Initialize and start a timer to process incoming serial data every 100 ms
+        /// </summary>
+        private void InitializeReceiveTimer()
+        {
+            receiveTimer = new System.Windows.Forms.Timer
+            {
+                Interval = 100 // Interval in milliseconds
+            };
+            receiveTimer.Tick += ReceiveTimer_Tick;
+            receiveTimer.Start();
+        }
+
+        /// <summary>
+        /// Timer event handler to process buffered incoming data if timeout elapsed
+        /// </summary>
+        private void ReceiveTimer_Tick(object sender, EventArgs e)
+        {
+            // Check if 100ms passed since last received data and buffer is not empty
+            if ((DateTime.Now - lastReceiveTime).TotalMilliseconds >= 100 && receiveBuffer.Length > 0)
+            {
+                string fullMessage = receiveBuffer.ToString();
+                receiveBuffer.Clear();
+                LogReceivedData(fullMessage);
+            }
+        }
+
+        /// <summary>
+        /// Populate the available serial ports into the comboBoxPorts and select defaults
+        /// </summary>
+        private void PopulateAvailablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxPorts.Items.Clear();
+            comboBoxPorts.Items.AddRange(ports);
+
+            if (ports.Length > 0)
+                comboBoxPorts.SelectedIndex = 0;
+
+            if (comboBoxBaudRate.Items.Count > 0)
+                comboBoxBaudRate.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Handles pressing Enter key in textBoxSend to trigger sending data if port is open
+        /// </summary>
+        private void TextBoxSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Prevent ding sound and new line
+
+                if (serialPort1.IsOpen)
+                {
+                    buttonSend.PerformClick();
+                }
+                else
+                {
+                    MessageBox.Show("Connection is not open. Cannot send data.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates status label and Send button enable state according to connection
+        /// </summary>
+        /// <param name="connected">Is serial port connected?</param>
+        /// <param name="portName">Connected port name</param>
+        /// <param name="baudRate">Connected baud rate</param>
+        private void UpdateConnectionStatus(bool connected, string portName, int baudRate)
+        {
+            if (connected)
+            {
+                toolStripStatusLabelConnection.Text = $"Connected to {portName} at {baudRate} baud";
+                buttonSend.Enabled = true;
+            }
+            else
+            {
+                toolStripStatusLabelConnection.Text = "No connection";
+                buttonSend.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Connect or disconnect the serial port based on current state
+        /// </summary>
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBoxPorts.SelectedItem.ToString();
+                    serialPort1.BaudRate = int.Parse(comboBoxBaudRate.SelectedItem.ToString());
+                    serialPort1.DataReceived += SerialPort1_DataReceived;
+                    serialPort1.Open();
+
+                    buttonConnect.Text = "Disconnect";
+                    MessageBox.Show("Connection established.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    UpdateConnectionStatus(true, serialPort1.PortName, serialPort1.BaudRate);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Connection error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    UpdateConnectionStatus(false, null, 0);
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    buttonConnect.Text = "Connect";
+                    MessageBox.Show("Connection closed.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    UpdateConnectionStatus(false, null, 0);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error closing connection: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Send the data from textBoxSend if the serial port is open
+        /// </summary>
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                MessageBox.Show("Connection is not open!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string dataToSend = textBoxSend.Text.Trim();
+            if (string.IsNullOrEmpty(dataToSend))
+                return;
+
+            try
+            {
+                serialPort1.WriteLine(dataToSend);
+                LogSentData(dataToSend);
+                textBoxSend.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Send error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Event handler for serial port data reception, append data to buffer and update last receive timestamp
+        /// </summary>
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string incomingData = serialPort1.ReadExisting();
+            receiveBuffer.Append(incomingData);
+            lastReceiveTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Clear the richTextBoxLog content
+        /// </summary>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBoxLog.Clear();
+        }
+
+        /// <summary>
+        /// Helper method to convert control characters (\r and \n) into visible notation
+        /// </summary>
+        /// <param name="data">Input string data</param>
+        /// <returns>String with control chars replaced</returns>
+        private string ShowControlChars(string data)
+        {
+            return data.Replace("\r", "[0x0D]").Replace("\n", "[0x0A]");
+        }
+
+        /// <summary>
+        /// Append a colored line of text to the richTextBoxLog and scroll to the bottom
+        /// </summary>
+        private void AppendLogLine(string text, Color color)
+        {
+            richTextBoxLog.SelectionStart = richTextBoxLog.TextLength;
+            richTextBoxLog.SelectionColor = color;
+            richTextBoxLog.AppendText(text + Environment.NewLine);
+            richTextBoxLog.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// Log sent data in richTextBoxLog with timestamp, visible control characters and byte count
+        /// </summary>
+        /// <param name="data">Sent data string</param>
+        private void LogSentData(string data)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+            string visibleData = ShowControlChars(data);
+            int byteCount = Encoding.ASCII.GetByteCount(data);
+            string logLine = $"[{timestamp}] TRANSMIT DATA: \"{visibleData}\", Total: {byteCount} byte";
+            AppendLogLine(logLine, Color.Lime);
+        }
+
+        /// <summary>
+        /// Log received data in richTextBoxLog with timestamp, visible control characters and byte count
+        /// </summary>
+        /// <param name="data">Received data string</param>
+        private void LogReceivedData(string data)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+            string visibleData = ShowControlChars(data);
+            int byteCount = Encoding.ASCII.GetByteCount(data);
+            string logLine = $"[{timestamp}] RECEIVE DATA: \"{visibleData}\", Total: {byteCount} byte";
+            AppendLogLine(logLine, Color.Cyan);
+        }
+    }
+}
